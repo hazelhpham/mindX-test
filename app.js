1296e0d2c884a8a995142db314ef1df610286950
Hazel H Pham <phd.h99@gmail.com>
2021-06-29 01:30:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
-const result = Math.floor( Math.random() * 20 ) +1; 
+let result = Math.floor( Math.random() * 20 ) +1; 
 let count = 0; 
-let userInput = parseInt(document.getElementById("user-input").value);
+
+// console.log(result);
 
 const submitButton = () => {
-     if((userInput > 20) || (userInput < 1)) { 
-        alert("please enter number from 1 to 20!") 
+    //get the value that user entered
+    let userInput = parseInt(document.getElementById("user-input").value);
+    
+    //conditions to check for valid input 
+    if((userInput > 20) || (userInput < 1) || (!Number.isInteger(userInput))) { 
+        alert("Please enter an Integer between 1 and 20 !") 
         return;
     }
-        if(userInput === result){
+    
+    if(userInput === result){
             alert("Correct number! You have a reward!");
+            result = Math.floor( Math.random() * 20 ) +1; 
             return;
         }
-        else if(userInput !== result){
+    else if(userInput !== result){
             alert("Wrong result. You can re-enter the number again!");
-            userInput = parseInt(document.getElementById("user-input").value);
+            userInput  = parseInt(document.getElementById("user-input").value);
             count++;
             if(count === 3){
+                //alert result
                 alert("Time's up. The correct number is " + result);
+                //reset the game
                 count = 0; 
+                result = Math.floor( Math.random() * 20 ) +1; 
                 return;
             }
-            if(userInput === result){
-                alert("Correct number! You have a reward!");
-                return;
-            }
-        }
+    }
         
-
-    userInput = parseInt(document.getElementById("user-input").value);
+    document.getElementById("user-input").value=""; //clear the input box 
     
 }
+
+
+    
 
 
     
